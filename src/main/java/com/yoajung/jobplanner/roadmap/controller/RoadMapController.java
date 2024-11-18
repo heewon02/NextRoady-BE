0d788e9532ff8ace7ce76a6c7bafdd55e4d5b5c6
whatsgoodg <rlwjddl1596@naver.com>
2024-11-18 14:05:41 +0900

    feat(RoadMapController): 컨트롤러 추가 및 구현
    아래의 서비스들의 요청을 처리하는 컨트롤러 추가 및 구현

---
+++ src/main/java/com/yoajung/jobplanner/roadmap/controller/RoadMapController.java
@@ -0,0 +1,56 @@
+package com.yoajung.jobplanner.roadmap.controller;
+
+import com.yoajung.jobplanner.roadmap.dto.request.JobValidationRequestDTO;
+import com.yoajung.jobplanner.roadmap.dto.request.OutlookRequestDTO;
+import com.yoajung.jobplanner.roadmap.dto.request.WholeRoadMapRequestDTO;
+import com.yoajung.jobplanner.roadmap.dto.request.YearRoadMapRequestDTO;
+import com.yoajung.jobplanner.roadmap.dto.response.JobValidationResponseDTO;
+import com.yoajung.jobplanner.roadmap.dto.response.OutlookResponseDTO;
+import com.yoajung.jobplanner.roadmap.dto.response.WholeRoadMapResponseDTO;
+import com.yoajung.jobplanner.roadmap.dto.response.YearRoadMapResponseDTO;
+import com.yoajung.jobplanner.roadmap.service.RoadMapService;
+import com.yoajung.jobplanner.roadmap.service.impl.JobValidationServiceImpl;
+import com.yoajung.jobplanner.roadmap.service.impl.OutlookServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/roadmap")
+public class RoadMapController {
+
+    private final RoadMapService roadMapService;
+    private final OutlookServiceImpl outlookServiceImpl;
+    private final JobValidationServiceImpl jobValidationServiceImpl;
+
+    @PostMapping("/whole")
+    public Mono<ResponseEntity<WholeRoadMapResponseDTO>> getWholeRoadMap(
+            @RequestBody WholeRoadMapRequestDTO requestDTO) {
+        Mono<WholeRoadMapResponseDTO> responseDTOMono = roadMapService.requestWholeRoadMap(requestDTO.job());
+        return responseDTOMono.map(ResponseEntity::ok);
+    }
+
+    @PostMapping("/year")
+    public Mono<ResponseEntity<YearRoadMapResponseDTO>> getYearRoadMap(@RequestBody YearRoadMapRequestDTO requestDTO) {
+        Mono<YearRoadMapResponseDTO> yearRoadMapMono = roadMapService.requestRoadMap(requestDTO.job());
+        return yearRoadMapMono.map(ResponseEntity::ok);
+    }
+
+    @PostMapping("outlook")
+    public Mono<ResponseEntity<OutlookResponseDTO>> getOutlook(@RequestBody OutlookRequestDTO requestDTO) {
+        Mono<OutlookResponseDTO> outlookResponseDTOMono = outlookServiceImpl.requestOutlookRequest(requestDTO.job());
+        return outlookResponseDTOMono.map(ResponseEntity::ok);
+    }
+
+    @PostMapping("validate")
+    public Mono<ResponseEntity<JobValidationResponseDTO>> validateJob(@RequestBody JobValidationRequestDTO requestDTO) {
+        Mono<JobValidationResponseDTO> jobValidationResponseDTOMono = jobValidationServiceImpl.validateJob(
+                requestDTO.job());
+        return jobValidationResponseDTOMono.map(ResponseEntity::ok);
+    }
+}
